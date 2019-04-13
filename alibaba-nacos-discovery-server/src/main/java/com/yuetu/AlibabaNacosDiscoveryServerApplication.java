b7dc44432b850e649debe7222545dd04c86fa071
姚超臣 <yaochaochen@ctv.org.cn>
2019-04-13 15:36:27 +0800

    通过API注入服务

--- alibaba-nacos-discovery-server/src/main/java/com/yuetu/AlibabaNacosDiscoveryServerApplication.java
+++ alibaba-nacos-discovery-server/src/main/java/com/yuetu/AlibabaNacosDiscoveryServerApplication.java
@@ -1,6 +1,10 @@
 package com.yuetu;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,14 +12,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableDiscoveryClient
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class AlibabaNacosDiscoveryServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlibabaNacosDiscoveryServerApplication.class, args);
     }
-    
+
+
+
+    // 注入 Nacos 的 NamingService 实例
+    @NacosInjected
+    private NamingService namingService;
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @PostConstruct
+    public void registerInstance() throws NacosException {
+        // 通过Nacos Open API 向 Nacos Server 注册一个名称为applicationName的服务
+        // curl -X PUT 'http://127.0.0.1:8848/nacos/v1/ns/instance?serviceName=XXX&ip=XXX&port=XXX'
+
+        namingService.registerInstance(applicationName, "127.0.0.1", serverPort);
+    }
     
     @Slf4j
     @RestController
